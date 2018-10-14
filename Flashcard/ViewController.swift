3b3c7ba44f1215f89cfc1212ec87b92305408d02
Martha C <macowner@imyodaddy.ddnszone.calstatela.edu>
2018-10-13 20:56:55 -0700

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Martha C. on 10/13/18.
+//  Copyright © 2018 codemartha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
